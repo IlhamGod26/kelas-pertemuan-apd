9447139468ee6a4eeb947099652b8043f05830ca
IlhamGod26 <ridwanali731@gmail.com>
2024-10-16 16:18:29 +0800

    upload pertemuan

---
+++ pertemuan-7/pertemuan7.py
@@ -0,0 +1,27 @@
+# def kali():
+#     x = 6*4
+#     print(x)
+
+# def luas_persegi_panjang(panjang, lebar): 
+#  luas = panjang * lebar 
+#  print ("Luas persegi panjang:", luas)  
+# luas_persegi_panjang(4, 6) 
+
+# def luas_persegi(sisi): 
+#     luas = sisi * sisi 
+#     jumlah = sisi + sisi
+#     return luas
+# luas, jumlah = luas_persegi(4)
+# print(luas)
+# print(jumlah)
+
+# def mhs(nama, nim, **arr) :
+#     print(nama)
+#     print(nim)
+#     print(arr)
+# mhs("Riduan", "114", data = 2, data2m = 3)
+
+def data(kumpulan):
+    print(kumpulan)
+value = [1,2,3,4,5]
+data(value)
